eacb1a21325e5c14118791c57267cc82569ecb96
xSminJx <151181757+xSminJx@users.noreply.github.com>
2024-11-10 18:49:31 +0900

    컴파일 해보셈

--- FPGA/Rx_Top.v
+++ FPGA/Rx_Top.v
@@ -1,3 +1,43 @@
 module Rx_Top(
-	
-)
+	i_Clk, i_Rst,
+    i_Rx,
+    o_FND0, o_FND1
+);
+
+input i_Clk, i_Rst;
+input i_Rx;
+
+output [6:0] o_FND0, o_FND1;
+
+wire [7:0] o_Data;
+wire o_fDone;
+
+reg [3:0] c_FND0, n_FND0,
+          c_FND1, n_FND1;
+
+FND FND0 (c_FND0, o_FND0);
+FND FND1 (c_FND1, o_FND1);
+
+UART_RX U0 (i_Clk, i_Rst, i_Rx, o_fDone, o_Data);
+
+always @(posedge i_Clk, negedge i_Rst) begin
+	if(!i_Rst) begin
+        c_FND0 = 0;
+        c_FND1 = 0;
+	end
+	else begin
+        c_FND0   = n_FND0;
+        c_FND1   = n_FND1;
+	end
+end
+
+always @* begin
+    n_FND0 = c_FND0;
+    n_FND1 = c_FND1;
+    if(o_fDone) begin
+        n_FND0 = o_Data[3:0];
+        n_FND1 = o_Data[7:4];
+    end
+end
+
+endmodule
