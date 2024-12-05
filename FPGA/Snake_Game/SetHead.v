a7f585ce6d0c5e817eb9fb17c57b7d2d26f11799
xSminJx <151181757+xSminJx@users.noreply.github.com>
2024-12-05 23:56:26 +0900

    Add files via upload

--- FPGA/Snake_Game/SetHead.v
+++ FPGA/Snake_Game/SetHead.v
@@ -6,7 +6,7 @@ module SetHead(
 
     input i_Clk, i_Rst;
     input [1:0] i_Way;
-    input [3:0] i_Push;
+    input [1:0] i_Push;
     input [6:0] i_Head_x, i_Head_y;
 
     output [6:0] o_Head_x, o_Head_y;
@@ -16,17 +16,13 @@ module SetHead(
               	RIGHT = 2,
               	LEFT  = 3;
 
-	wire isUnway = (!i_Push[UP] && i_Way==DOWN) ||
-				     (!i_Push[DOWN] && i_Way==UP) ||
-				     (!i_Push[RIGHT] && i_Way==LEFT) ||
-				     (!i_Push[LEFT] && i_Way==RIGHT);
+	wire isUnway = (i_Push==UP && i_Way==DOWN) ||
+				   (i_Push==DOWN && i_Way==UP) ||
+				   (i_Push==RIGHT && i_Way==LEFT) ||
+				   (i_Push==LEFT && i_Way==RIGHT);
 
-    wire [1:0] Push = !i_Push[0] ? 0 :
-                      !i_Push[1] ? 1 :
-                      !i_Push[2] ? 2 : 3;
-
-    assign o_Way = isUnway ? i_Way : Push;
-    assign o_Head_x = isUnway ? i_Head_x + (i_Way == UP ? -1 : (i_Way == DOWN ? 1 : 0)) : i_Head_x + (!i_Push[UP] ? -1 : (!i_Push[DOWN] ? 1 : 0));
-    assign o_Head_y = isUnway ? i_Head_y + (i_Way == LEFT ? -1 : (i_Way == RIGHT ? 1 : 0)) : i_Head_x + (!i_Push[LEFT] ? -1 : (!i_Push[RIGHT] ? 1 : 0));
+    assign o_Way = isUnway ? i_Way : i_Push;
+    assign o_Head_x = isUnway ? i_Head_x + (i_Way == UP ? -1 : (i_Way == DOWN ? 1 : 0)) : i_Head_x + (i_Push == UP ? -1 : (i_Push == DOWN ? 1 : 0));
+    assign o_Head_y = isUnway ? i_Head_y + (i_Way == LEFT ? -1 : (i_Way == RIGHT ? 1 : 0)) : i_Head_x + (i_Push == LEFT ? -1 : (i_Push == RIGHT ? 1 : 0));
 
 endmodule
--- FPGA/Snake_Game/snake_game.v
+++ FPGA/Snake_Game/snake_game.v
@@ -14,6 +14,7 @@ module Snake_Game (
 
     parameter XSIZE     = 60,
               YSIZE     = 80,
+              MAX_SIZE  = XSIZE*YSIZE,
               LST_CLK   = 25_000_000,
 
               IDLE      = 3'b000,
@@ -28,28 +29,31 @@ module Snake_Game (
 
     //좌표 0, 0은 NULL역할
     reg [24:0] c_ClkCnt, n_ClkCnt;
-    reg [6:0]  c_Body_x[XSIZE*YSIZE], n_Body_x[XSIZE*YSIZE], // 뱀의 몸통을 저장할 배열(큐 역할)
-               c_Body_y[XSIZE*YSIZE], n_Body_y[XSIZE*YSIZE]; 
+    reg [6:0]  c_Body_x[0:MAX_SIZE-1], n_Body_x[0:MAX_SIZE-1], // 뱀의 몸통을 저장할 배열(큐 역할)
+               c_Body_y[0:MAX_SIZE-1], n_Body_y[0:MAX_SIZE-1]; 
     reg [6:0]  c_Head_x, n_Head_x,   // 뱀의 머리 위치 저장
                c_Head_y, n_Head_y;
     reg [6:0]  c_Item_x, n_Item_x,   // 아이템 위치 저장
                c_Item_y, n_Item_y;
     reg [8:0]  c_Size, n_Size;       // 뱀 크기(점수랑 같은 역할)
-    reg [1:0]  c_Way, n_Way;         // 뱀이 움직였던 방향
+    reg [1:0]  c_Way, n_Way,         // 뱀이 움직였던 방향
+               c_Push, n_Push;       // 조이스틱 입력 저장
     reg [2:0]  c_State, n_State;
     reg [4:0]  c_Speed, n_Speed;
     reg [4:0]  c_SpdTimeCnt, n_SpdTimeCnt; // 먹이를 먹으면 일정 시간동안 속도가 빨라지는데, 그 일정시간을 저장할 레지스터
 
-    wire o_Head_x, o_Head_y;
-    wire [1:0] o_Way;
-
+    wire [6:0] SH_o_Head_x, SH_o_Head_y;
+    wire [1:0] SH_o_Way;
+    wire [6:0] SB_o_Body_x[0:MAX_SIZE-1], SB_o_Body_y[0:MAX_SIZE-1];
+    
     wire isLstClk = c_ClkCnt <= LST_CLK;
     wire isEat = (n_Head_x == c_Item_x && n_Head_y == c_Item_y) && c_State == CHANGE;
-    wire isGameOver = (n_Head_x == 0 || n_Head_y == 0 || n_Heao_x > XSIZE + 1 || n_Head_y > YSIZE); // 그리고 몸통에 박았는지도 확인(병렬처리하셈 아니면 모듈 만들던가)
+    wire isGameOver = (n_Head_x == 0 || n_Head_y == 0 || n_Head_x > XSIZE + 1 || n_Head_y > YSIZE); // 그리고 몸통에 박았는지도 확인(병렬처리하셈 아니면 모듈 만들던가)
     wire isSpdDw = c_SpdTimeCnt == 16;
 
     //사이즈값(2진수) -> FND로 변환하는거 추가해야함(스탑워치에서 했던거 써서 모듈로 따로 분리하면 될듯)
 
+    integer j;
     always @(posedge i_Clk or negedge i_Rst) begin
         if(!i_Rst) begin
             c_ClkCnt     = 0;
@@ -58,25 +62,39 @@ module Snake_Game (
             c_Item_x     = 0;
             c_Item_y     = 0;
             c_Size       = DEF_SIZE;
-            c_Way        = 0; // 상하좌우를 제외한 값(4는 움직임X)
+            c_Way        = 0;
+            c_Push       = 0;
             c_State      = IDLE;
             c_Speed      = DEF_SPD;
             c_SpdTimeCnt = 0;
+
+            for(j=0;j<DEF_SIZE;j=j+1) begin
+                c_Body_x[i]=c_Head_x;
+                c_Body_y[i]=c_Head_y;
+            end
         end else begin
             c_ClkCnt     = n_ClkCnt;
             c_Head_x     = n_Head_x;
             c_Head_y     = n_Head_y;
             c_Size       = n_Size;
             c_Way        = n_Way;
-            c_Body       = n_Body;
+            c_Push       = n_Push;
             c_State      = n_State;
             c_Speed      = n_Speed;
             c_SpdTimeCnt = n_SpdTimeCnt;
+
+            for(j=0;j<MAX_SIZE;j=j+1) begin
+                c_Body_x[i]=n_Body_x[i];
+                c_Body_y[i]=n_Body_y[i];
+            end
         end
     end
 
-    SetHead S0 (i_Clk, i_Rst, c_Way, i_Push, c_Head_x, c_Head_y, o_Head_x, o_Head_y, o_Way);
+    SetHead S0 (i_Clk, i_Rst,
+                c_Way, c_Push, c_Head_x, c_Head_y,
+                SH_o_Head_x, SH_o_Head_y, SH_o_Way);
 
+    integer i;
     always @* begin
         n_Head_x = c_Head_x;
         n_Head_y = c_Head_y;
@@ -85,6 +103,7 @@ module Snake_Game (
         n_Size   = c_Size;
         n_ClkCnt = 0;
         n_Way    = c_Way;
+        n_Push   = c_Push;
         n_State  = c_State;
         n_Speed  = c_Speed;
         n_SpdTimeCnt = c_SpdTimeCnt;
@@ -92,21 +111,26 @@ module Snake_Game (
         case(c_State)
             IDLE : begin
                 n_ClkCnt = c_ClkCnt + 1;
-                if(!&i_Push) begin
+                if(!(&i_Push)) begin
                     n_State = RUN;
                     n_ClkCnt = 0;
                 end
             end
             RUN  : begin
                 n_ClkCnt = isLstClk ? 0 : c_ClkCnt + c_Speed;
+                n_Push = !i_Push[0] ? 0 :
+                         !i_Push[1] ? 1 :
+                         !i_Push[2] ? 2 :
+                         !i_Push[3] ? 3 : c_Push;
+
                 if(isLstClk) begin
                     //속도 조절 로직
                     n_SpdTimeCnt = isSpdDw ? 0 : c_SpdTimeCnt + 1;
-                    n_Speed = isSpdDw ? DEF_SPD : c_Speed;
+                    n_Speed = isSpdDw ? DEF_SPD : c_Speed; // 시간 다되면 기본 속도
 
                     //헤드 위치, 방향 갱신
-                    {n_Head_x,n_Head_y} = {o_Head_x,o_Head_y};
-                    n_Way = o_Way;
+                    {n_Head_x,n_Head_y} = {SH_o_Head_x,SH_o_Head_y};
+                    n_Way = SH_o_Way;
 
                     n_State = CHANGE;
                 end
@@ -123,8 +147,18 @@ module Snake_Game (
                 n_State = o_isMakeItem_Done ? (isGameOver ? STOP : SETQU) : c_State;
             end
             SETBODY : begin // 사이즈값 갱신했으니 그걸로 몸통 큐 갱신
-                //{n_Body_x,n_Body_y} = ; // 사이즈값, 현재 몸통 큐 받아서 큐 갱신하는 함수 만드셈
-                n_State = isSetBody_Done ? RUN : c_State;
+                n_Body_x[0] = c_Head_x;
+                n_Body_y[0] = c_Head_y;
+                for(i = 0; i < MAX_SIZE - 1; i = i + 1) begin
+                    if(i < c_Size) begin
+                        n_Body_x[i+1] = c_Body_x[i];
+                        n_Body_y[i+1] = c_Body_y[i];
+                    end else begin
+                        n_Body_x[i+1] = c_Body_x[c_Size-1];
+                        n_Body_y[i+1] = c_Body_y[c_Size-1];
+                    end
+                end
+                n_State = RUN;
             end
             PAUSE : begin
                 n_State = !i_Pause ? RUN : c_State;
